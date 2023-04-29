cafebc4c1aa034c13642d077d06db561d06bc971
Taylor Burks <tayyy25@gmail.com>
2023-04-29 19:36:11 -0400

    the dom modify

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Maddie is the champion";
+document.body.append(newHeader);
+
